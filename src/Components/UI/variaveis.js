dc333f1d91ec918b2b7a0492a252b5315a54a689
GiovannaValadao <giovannnavsousa04@gmail.com>
2022-04-20 13:06:16 -0300

    create componentes with styled components

---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,8 @@
+export const corPrimaria = "#41d3be";
+export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "white";
+export const textoFundoClaro = "grey";
+
+export const fundoEscuro = "#5c5b5e";
+export const conteudoEscuro = "5e5e5e";
+export const textoFundooEscuro = "#FAFAFA";
